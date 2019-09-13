c7f2418cc0602a840904d8d6edf36c7ab6b3159d
zcoursey22 <zcoursey22@gmail.com>
2019-09-12 23:37:38 -0400

    Makes post thumbnails look better

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ export const pageQuery = graphql`
           publishDate(formatString: "MMMM Do, YYYY")
           tags
           heroImage {
-            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: FILL) {
               ...GatsbyContentfulFluid
             }
           }
